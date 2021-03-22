b25b90d065b9f340a5db9e11d194edf0d5a7692f
OrphanMaker3 <79663350+OrphanMaker3-rgb@users.noreply.github.com>
2021-03-22 17:17:18 -0500

    Adding File
    This is a simple addition calculator

---
+++ Addition Calc.py
@@ -0,0 +1,9 @@
+import time
+
+# Addition
+num1 = float(input("Enter the first number that you want to add: "))
+num2 = float(input("Enter the second number that you want to add: "))
+print("Loading...")
+time.sleep(0.2)
+total = num1 + num2
+print("Total: ", total)
