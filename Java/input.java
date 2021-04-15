291bf5e63906ed1d380282bc636f1fde1f648911
OrphanMaker3 <79663350+OrphanMaker3-rgb@users.noreply.github.com>
2021-04-15 16:51:44 -0500

    change location
    das it

---
+++ Java/input.java
@@ -0,0 +1,33 @@
+package Java;
+import java.io.*;
+
+public class input{
+    
+    public static void calculator() throws IOException {
+               
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         
+         String input = br.readLine();
+         int intInput = Integer.parseInt(input);
+
+
+         String intInputtwo = br.readLine();
+         int intInput2 = Integer.parseInt(intInputtwo);
+
+        System.out.println(intInput2 + intInput);
+   
+   
+   
+   
+   
+    }  
+    
+    
+    // function calling
+    public static void main(String[] args) throws IOException{
+    
+        //call meathod below
+        calculator();
+ } 
+}
+
